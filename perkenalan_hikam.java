62ba12066cc29c0763a3f83017d55d2be8b0d0c4
tugas <aldayanuar6@gmail.com>
2024-09-07 10:07:12 +0700

    perkenalan kelompok saya

---
+++ perkenalan_hikam.java
@@ -0,0 +1,11 @@
+public class perkenalan_hikam {
+
+    public static void main(String[] args) {
+        System.out.println("===== Biodata hikam =====");
+        System.out.println("Nama: muhammad aklilul hikam ");
+        System.out.println("Umur: 19 tahun");
+        System.out.println("Jurusan: Teknologi Informasi");
+        System.out.println("Hobi: Desain");
+
+    }
+}
